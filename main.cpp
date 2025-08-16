7145fa276654c7316664fb254cf779729989567c
rocklake <benrgentry@gmail.com>
2025-08-16 15:05:33 -0400

    add back fix bugs

--- main.cpp
+++ main.cpp
@@ -4,7 +4,6 @@
 #include <string>
 #include <vector>
 #include <utility>
-
 std::pair<std::string, std::string> sp(const std::string& input) {
     size_t colonPos = input.find(':');
     size_t semicolonPos = input.find(';');
@@ -24,6 +23,36 @@ std::pair<std::string, std::string> sp(const std::string& input) {
         // No delimiter found: return whole string as "before", empty string as "after"
         return {input, ""};
     }
+}
+std::pair<int, int> get_b_count(const std::vector<std::string>& og, int lnum, int bback) {
+    int armbytes = 0;
+    int x86bytes = 0;
+
+    const int rl_x86 = 30, rl_arm = 5;
+    const int sc_x86 = 10, sc_arm = 1;
+    const int ed_x86 = 20, ed_arm = 3;
+
+    // Loop over the last `bback` lines before `lnum`
+    for (int i = lnum - bback; i < lnum; ++i) {
+        if (i < 0 || i >= static_cast<int>(og.size())) continue;
+
+        std::pair<std::string, std::string> parts = sp(og[i]);
+        std::string first = parts.first;
+        std::string second = parts.second;
+
+        if (first == "raw_log") {
+            x86bytes += rl_x86;
+            armbytes += rl_arm;
+        } else if (first == "syscall") {
+            x86bytes += sc_x86;
+            armbytes += sc_arm;
+        } else if (first == "end") {
+            x86bytes += ed_x86;
+            armbytes += ed_arm;
+        }
+    }
+
+    return {x86bytes, armbytes};
 }
 int main(int argc, char* argv[]) {
     // config
@@ -57,8 +86,9 @@ int main(int argc, char* argv[]) {
     }
 
     int counter = 0;
+    
     while (counter < og.size()) {
-         auto instruction = sp(og[counter]);
+        auto instruction = sp(og[counter]);
         if (instruction.first == "end") {
             // linux
             textL64 = textL64 + tab + "mov rax,60\n";
@@ -77,34 +107,35 @@ int main(int argc, char* argv[]) {
         }
         if (instruction.first == "raw_log") {
             // linux
-            // v{name}: db {value}
             dataL64 = dataL64 + tab + "v" + std::to_string(counter) + ": db " + instruction.second + "\n";
-            // c{name}: equ $ - {prename}
             dataL64 = dataL64 + tab + "c" + std::to_string(counter) + ": equ $ - " + "v" + std::to_string(counter) + "\n";
-            // mov rax,1
-            // mov rdi,1
-            // mov rsi,name
-            // mov rdx,namelen
-            // syscall
             textL64 = textL64 + tab + "mov rax,1\n";
             textL64 = textL64 + tab + "mov rdi,1\n";
             textL64 = textL64 + tab + "mov rsi,v" + std::to_string(counter) + "\n";
             textL64 = textL64 + tab + "mov rdx,c" + std::to_string(counter) + "\n";
             textL64 = textL64 + tab + "syscall\n";
             // linux arm
-            // v{cuount}:	.asciz {value}
             dataA64 = dataA64 + tab + "v" + std::to_string(counter) + ": .asciz " + instruction.second + "\n";
-            //mov	x0, #1
-            //ldr	x1, =v{cuount}
-            //mov	x2, #3
-            //mov	x8, #64
-            //svc	#0 syscall
+            dataA64 = dataA64 + tab + "c" + std::to_string(counter) + " = . - " + "v" + std::to_string(counter) + "\n";
+            textA64 = textA64 + tab + "mov	x8, #64\n";
             textA64 = textA64 + tab + "mov	x0, #1\n";
             textA64 = textA64 + tab + "ldr	x1, =v" + std::to_string(counter) + "\n";
-            textA64 = textA64 + tab + "mov	x2, #3\n";
-            textA64 = textA64 + tab + "mov	x8, #64\n";
+            textA64 = textA64 + tab + "mov	x2, c" + std::to_string(counter) + "\n";
             textA64 = textA64 + tab + "svc	#0\n";
-
+        }
+        if (instruction.first == "back") {
+            int backc = std::stoi(instruction.second);
+            std::pair<int, int> result = get_b_count(og, counter, backc);
+            int x86bytes = result.first;
+            int armbytes = result.second;
+            if (x86bytes > 2147483647 or armbytes > 134217727) {
+                std::cout << "Line " << std::to_string(counter) << "\nCan not jump back more the 134217727 bytes or ~19173961 pyro lines.\nps. question what you're doing bc that a huge jump\nExiting...\n";
+                return EXIT_FAILURE;
+            }
+            //linux
+            textL64 = textL64 + tab + "jmp near -" + std::to_string(x86bytes) + "\n";
+            //linux arm
+            textA64 = textA64 + tab + "b #-" + std::to_string(armbytes) + "\n";
         }
         counter++;
 
@@ -123,5 +154,5 @@ int main(int argc, char* argv[]) {
     A64 << dataA64 + "\n";
     A64 << textA64 + "\n";
     A64.close();
-    return 0;
+    return EXIT_SUCCESS;
 }
